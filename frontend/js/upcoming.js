810158d22832b7fcb7a0cfe4d02bf7c310896049
wolflorena <wolflorenaani@gmail.com>
2023-05-27 00:21:40 +0300

    Connected fe with be for upcoming page

--- frontend/js/upcoming.js
+++ frontend/js/upcoming.js
@@ -7,9 +7,56 @@ if (!id) {
 
 const welcomeMessage = document.getElementById("welcome-message");
 
+const portUrl = "https://localhost:7053";
+const showsUrl = "/api/shows";
+const url = portUrl + showsUrl;
+
 var username_message = document.createElement("span");
 username_message.innerHTML = " " + username;
 welcomeMessage.appendChild(username_message);
+
+const getShows = document.getElementById("cards");
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    getShow(data);
+  });
+
+function getShow(shows) {
+  let upcomingShowSort = [...shows].sort((s1, s2) =>
+    s1.daysUntilNewSeason > s2.daysUntilNewSeason
+      ? 1
+      : s1.daysUntilNewSeason < s2.daysUntilNewSeason
+      ? -1
+      : 0
+  );
+
+  upcomingShowSort.forEach((show) => {
+    let find = "tvshow.html?" + show.id;
+    let upcoming = `<a href="${find}">
+                    <div class="card">
+                        <img src="img/${show.poster}" alt="" />
+                        <div class="card-text-center">
+                          <h3>${show.daysUntilNewSeason} days</h3>
+                        </div>
+                        <h3>${show.name}</h3>
+                        <h5>Season ${show.seasons + 1} ○ ${show.newSeason}</h5>
+                    </div>
+                   </a>`;
+
+    if (
+      getShows != null &&
+      show.daysUntilNewSeason > 0 &&
+      show.daysUntilNewSeason < 101
+    ) {
+      const getShows = document.getElementById("cards");
+      const newUpcoming = document.createElement("a");
+      newUpcoming.innerHTML = upcoming;
+      getShows.appendChild(newUpcoming);
+    }
+  });
+}
 
 function logout() {
   sessionStorage.clear();
--- frontend/js/watchlist.js
+++ frontend/js/watchlist.js
@@ -5,17 +5,85 @@ if (!id) {
   window.location = "login.html";
 }
 
-//const portUrl = "https://localhost:7053";
-//const showsUrl = "/api/shows";
-//const url = portUrl + showsUrl;
-const getShowsPopularContainer = document.getElementById("cards-popular");
-const getShowsExploreContainer = document.getElementById("cards-explore");
+const portUrl = "https://localhost:7053";
+const showsUrl = "/api/shows/";
+const watchedUrl = "/api/watched/";
+const url = portUrl + watchedUrl + id;
+const addedUrl = portUrl + "/api/added/" + id;
+
+const getShowsCurrentlyContainer = document.getElementById("cards-watching");
+const getShowsNotStarted = document.getElementById("cards-notStarted");
 const welcomeMessage = document.getElementById("welcome-message");
 
 var username_message = document.createElement("span");
 username_message.innerHTML = " " + username;
 welcomeMessage.appendChild(username_message);
 
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     getShow(data);
+//   });
+
+// function getShow(shows) {
+//   let countWatching = 1;
+
+//   shows.forEach((show) => {
+
+//     let find = "tvshow.html?" + show.id;
+//     let currently = `<a href="#">
+//                         <div class="card">
+//                           <img src="img/card_arcane.png" alt="" />
+//                           <div class="pie-container">
+//                             <div class="pie" style="--p: 90">90%</div>
+//                           </div>
+//                           <h3>Arcane</h3>
+//                         </div>
+//                       </a>`;
+
+//     if (getShowsCurrentlyContainer != null && countWatching <= 5) {
+//       const getShowsCurrentlyContainer =
+//         document.getElementById("cards-watching");
+//       const newWatching = document.createElement("a");
+//       newWatching.innerHTML = currently;
+//       getShowsCurrentlyContainer.appendChild(newWatching);
+//       countWatching++;
+//     }
+//   });
+// }
+
+fetch(addedUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    getNotStarted(data);
+  });
+
+function getNotStarted(shows) {
+  let countNotStarted = 1;
+
+  shows.forEach((show) => {
+    fetch(portUrl + showsUrl + show.tvShowId)
+      .then((res) => res.json())
+      .then((data) => {
+        let find = "tvshow.html?" + data.id;
+        let notStarted = `<a href="${find}">
+                        <div class="card">
+                          <img src="img/${data.poster}" alt="" />
+                          <h3>${data.name}</h3>
+                        </div>
+                      </a>`;
+
+        if (getShowsNotStarted != null && countNotStarted <= 5) {
+          const getShowsNotStarted =
+            document.getElementById("cards-notStarted");
+          const newNotStarted = document.createElement("a");
+          newNotStarted.innerHTML = notStarted;
+          getShowsNotStarted.appendChild(newNotStarted);
+          countNotStarted++;
+        }
+      });
+  });
+}
 function logout() {
   document.location.href = "login.html";
   sessionStorage.clear();
